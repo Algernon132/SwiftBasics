04dd1909f4fa8ce4b3e0f447338142c93ea80b89
Chaz <c.bruscato97@gmail.com>
2018-08-30 16:49:41 -0500

    start
    Declared all initial variables and started other requirements

--- SwiftBasics/main.swift
+++ SwiftBasics/main.swift
@@ -8,5 +8,43 @@
 
 import Foundation
 
-print("Hello, World!")
+
+
+let sample1 : uint8 = 0x3A
+var sample2 : uint8 = 58
+var heartRate : Int = 85
+var deposits : Double = 135002796
+let acceleration : Float = 9.800
+var mass : Float = 14.6
+var distance : Double = 129.763001
+var lost : Bool = true
+var expensive : Bool = true
+var choice : Int = 2
+let integral : Character = "\u{222B}"
+let greeting : String = "Hello"
+var name : String = "Karen"
+
+
+if sample1 == sample2{
+    print("The samples are equal.")
+}else{
+    print("The samples are not equal.")
+}
+
+if heartRate >= 40 && heartRate >= 80{
+    print("Heart rate is normal.")
+}else{
+    print("Heart rate is not normal.")
+}
+
+if deposits >= 100000000{
+    print("You are exceedingly wealthy.")
+}else{
+    print("Sorry you are so poor.")
+}
+
+var force = mass * acceleration    //implicitly defined
+
+print("force = \(force)")
+
 
